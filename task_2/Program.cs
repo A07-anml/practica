fd5776c634888169e4a4dff30b7b9df3e8d65157
Mashishkash <a07thelittlegirl@gmail.com>
2025-06-07 02:18:10 +0700

    first commit

---
+++ task_2/Program.cs
@@ -0,0 +1,26 @@
+namespace task_2;
+
+public static class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Programs:");
+            Console.WriteLine("[Q] - Finish");
+            string? command = Console.ReadLine();
+            switch (command.ToUpper())
+            {
+                case "Q":
+                    Console.Clear();
+                    Console.WriteLine("Finish");
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    continue;
+            }
+
+            break;
+        }
+    }
+}
